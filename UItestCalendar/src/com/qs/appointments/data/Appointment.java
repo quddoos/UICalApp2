4a26bebadee2057ec8b971aa03c40c96c02dbec4
quddoos <quddoos_khan@yahoo.com>
2017-01-10 21:58:43 -0500

    test project

---
+++ UItestCalendar/src/com/qs/appointments/data/Appointment.java
@@ -0,0 +1,19 @@
+package com.qs.appointments.data;
+
+public class Appointment {
+
+	private String appointmentId;
+	private String appointmentDetail;
+	public String getAppointmentId() {
+		return appointmentId;
+	}
+	public void setAppointmentId(String appointmentId) {
+		this.appointmentId = appointmentId;
+	}
+	public String getAppointmentDetail() {
+		return appointmentDetail;
+	}
+	public void setAppointmentDetail(String appointmentDeatil) {
+		this.appointmentDetail = appointmentDeatil;
+	}
+}
